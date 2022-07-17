e0918529bba03f40a0efb6f6dfd02d5a5e5912bc
Faith <faiththe1rst@gmail.com>
2022-07-17 15:08:52 +0100

    combinedProvider issue

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,3 +1,3 @@
-import CombinedProvider from './src/context/CombinedProvider.js';
+import CombinedProvider from './src/context/CombinedProvider';
 
 export const wrapRootElement = CombinedProvider
--- src/context/combinedProvider.tsx
+++ src/context/combinedProvider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Context } from './context';
 
-const CombinedProvider = ({ element }) => {
+const CombinedProvider = ({ element }: any) => {
   return <Context>{element}</Context>;
 };
 
